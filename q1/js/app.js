51cf21a9899ba5adc9305c5e53c75837c11c9425
Zyril Del Rosario <mitski.zyril@gmail.com>
2020-12-22 10:31:03 -0800

    init repo

---
+++ q1/js/app.js
@@ -0,0 +1,19 @@
+particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
+// window.onscroll = function() {stickyNav()};
+
+// var nav = document.getElementById("nav");
+
+// var sticky = nav.offsetTop;
+
+// function stickyNav() {
+//     if (window.pageYOffset >= sticky) {
+//         nav.classList.add("sticky");
+//     } else {
+//         nav.classList.remove("sticky");
+//     }
+// }
+
+// window.onload = window.alert("Hello! \n\nThank you for testing my portfolio page. This is currently in development and it will needs some work. \n\nAny issues please report it here: \nhttps://github.com/msyagami/sy-2020-presentation. \nThank you!")
