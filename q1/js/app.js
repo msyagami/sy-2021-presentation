82398349831eb542d1f86427bf75167d2d396e53
Zyril Del Rosario <mitski.zyril@gmail.com>
2021-03-08 01:14:55 -0800

    this might fix break on css

--- q1/js/app.js
+++ q1/js/app.js
@@ -16,7 +16,7 @@ particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
 //     }
 // }
 
-// window.onload = window.alert("Hello! \n\nThank you for testing my portfolio page. This is currently in development and it will needs some work. \n\nAny issues please report it here: \nhttps://github.com/msyagami/sy-2020-presentation. \nThank you!")
+// window.onload = window.alert("Hello! \n\nThank you for testing my portfolio page. This is currently in development and it will needs some work. \n\nAny issues please report it here: \nhttps://github.com/msyagami/sy-2021-presentation. \nThank you!")
 
 function openMobileNav() {
   var x = document.getElementById("nav");
